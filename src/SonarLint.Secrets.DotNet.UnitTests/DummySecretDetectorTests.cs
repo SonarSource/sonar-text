b5059936606a9d1e7262a8d0c2a7c1d9d8ed5a96
duncanp-sonar <duncan.pocklington@sonarsource.com>
2021-10-05 10:16:50 +0100

    Add dummy detector implementation (#28)

--- src/SonarLint.Secrets.DotNet.UnitTests/DummySecretDetectorTests.cs
+++ src/SonarLint.Secrets.DotNet.UnitTests/DummySecretDetectorTests.cs
@@ -4,8 +4,10 @@
  * mailto:info AT sonarsource DOT com
  */
 
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SonarLint.VisualStudio.Integration.UnitTests;
+using System.Linq;
 
 namespace SonarLint.Secrets.DotNet.UnitTests
 {
@@ -16,6 +18,30 @@ namespace SonarLint.Secrets.DotNet.UnitTests
         public void MefCtor_CheckIsExported()
         {
             MefTestHelpers.CheckTypeCanBeImported<DummySecretDetector, ISecretDetector>(null, null);
+        }
+
+        [TestMethod]
+        public void Find_Simple()
+        {
+            var testSubject = new DummySecretDetector();
+
+            const string input = @"password=123
+  woijwoj w woijw e
+  ffef  password=abcdef
+
+";
+
+            var actual = testSubject.Find(input);
+
+            actual.Count().Should().Be(2);
+
+            actual.First().RuleKey.Should().Be("secrets:DUMMY");
+            actual.First().StartIndex.Should().Be(9);
+            actual.First().EndIndex.Should().Be(11);
+
+            actual.Last().RuleKey.Should().Be("secrets:DUMMY");
+            actual.Last().StartIndex.Should().Be(52);
+            actual.Last().EndIndex.Should().Be(57);
         }
     }
 }
