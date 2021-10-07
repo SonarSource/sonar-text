59c1829f9f3adbf70209558258fbc60d1127dd3e
duncanp-sonar <duncan.pocklington@sonarsource.com>
2021-10-07 13:31:50 +0100

    [.NET] Add core matching classes and tests (#36)
    * [.NET] Add core matching classes and tests

---
+++ src/SonarLint.Secrets.DotNet/DotNetOnly/TextRange.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Copyright (C) 2018-2021 SonarSource SA
+ * All rights reserved
+ * mailto:info AT sonarsource DOT com
+ */
+
+// The Java code uses a TextRange class defined in the Sonar API (org.sonar.api.batch.fs.TextRange).
+
+using System.Diagnostics;
+
+namespace SonarLint.Secrets.DotNet.DotNetOnly
+{
+    internal class TextRange
+    {
+        public TextRange(int start, int end)
+        {
+            Debug.Assert(start >= 0, "Start index should be greater than zero");
+            Debug.Assert(end >= start, "End index should be equal to or after the end index");
+
+            StartIndex = start;
+            EndIndex = end;
+        }
+
+        public int StartIndex { get; }
+        public int EndIndex { get; }
+
+        public bool overlap(TextRange other) =>
+            IndexIsInRange(StartIndex, other) ||
+            IndexIsInRange(EndIndex, other) ||
+            RangeIsCompletelyContained(other, this);
+
+        private static bool IndexIsInRange(int index, TextRange other) =>
+            index >= other.StartIndex && index <= other.EndIndex;
+
+        private static bool RangeIsCompletelyContained(TextRange candidateRange, TextRange possibleContainer) =>
+            (possibleContainer.StartIndex < candidateRange.StartIndex && possibleContainer.EndIndex > candidateRange.EndIndex);
+    }
+}
---
+++ src/SonarLint.Secrets.DotNet/Rules/Matching/Match.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Copyright (C) 2018-2021 SonarSource SA
+ * All rights reserved
+ * mailto:info AT sonarsource DOT com
+ */
+
+// NOTE: the Match API in .NET is not the same as the Java version
+
+namespace SonarLint.Secrets.DotNet.Rules.Matching
+{
+    internal class Match
+    {
+        public Match(string text, int startIndex, int length) {
+            Text = text;
+            StartIndex = startIndex;
+            Length = length;
+        }
+
+        public string Text { get; }
+        public int StartIndex{ get; }
+        public int Length { get; }
+    }
+}
