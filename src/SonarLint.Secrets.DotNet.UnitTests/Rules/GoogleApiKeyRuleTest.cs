7622c5068b8466bab5b7c5bb1fd2e0f773f453e1
duncanp-sonar <duncan.pocklington@sonarsource.com>
2021-10-08 09:04:54 +0100

    [.NET] Implement S6334, S6335, S6337 (#40)
    * Implement S6334, S6335, S6337
    
    * S6334 - Google API keys #2674
    * S6335 - Google Cloud service account keys #2675
    * S6337 - IBM API keys #2676

---
+++ src/SonarLint.Secrets.DotNet.UnitTests/Rules/GoogleApiKeyRuleTest.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * Copyright (C) 2018-2021 SonarSource SA
+ * All rights reserved
+ * mailto:info AT sonarsource DOT com
+ */
+
+// Ported from ...\sonar-secrets-plugin\src\test\java\com\sonarsource\secrets\rules\GoogleApiKeyRuleTest.java
+
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SonarLint.Secrets.DotNet.Rules;
+using SonarLint.VisualStudio.Integration.UnitTests;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SonarLint.Secrets.DotNet.UnitTests.Rules
+{
+    [TestClass]
+    public class GoogleApiKeyRuleTest
+    {
+        [TestMethod]
+        public void MefCtor_CheckIsExported()
+        {
+            MefTestHelpers.CheckTypeCanBeImported<GoogleApiKeyRule, ISecretDetector>(null, null);
+        }
+
+        [TestMethod]
+        public void TestRuleProperties()
+        {
+            var testSubject = new GoogleApiKeyRule();
+
+            testSubject.RuleKey.Should().Be("S6334");
+            testSubject.Name.Should().Be("Google API keys should not be disclosed");
+            testSubject.Message.Should().Be("Make sure this Google API Key is not disclosed.");
+        }
+
+        [TestMethod]
+        public void TestRuleRegexPositive()
+        {
+            var testSubject = new GoogleApiKeyRule();
+
+            var secrets = testSubject.Find("android:value=\"AIzaSyCis4NzxMw1aJyvUIrjGILjPkSdxrRfof4\"");
+
+            secrets.Count().Should().Be(1);
+
+            // The Java test checks for the tuple(1, 15, 1, 54) (start.line, start.lineOffset, end.line, end.lineOffset) 
+            secrets.First().StartIndex.Should().Be(15);
+            secrets.First().Length.Should().Be(39);
+        }
+
+        [TestMethod]
+        public void TestRuleRegexNegative()
+        {
+            var testSubject = new GoogleApiKeyRule();
+
+            IEnumerable<ISecret> secrets = testSubject.Find("android:value=\"AIzaSyCis4NzxMw1aJyvUIrjGfof4\"");
+
+            secrets.Should().BeEmpty();
+        }
+    }
+}
