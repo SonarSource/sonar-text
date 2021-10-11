1af8dd7b3f65583ea35fe5724feaa0bde498daf7
duncanp-sonar <duncan.pocklington@sonarsource.com>
2021-10-11 17:01:56 +0100

    [.NET] Added S6338 - Azure Storage Account Keys (#42)
    * [.NET] Added S6338 - Azure Storage Account Keys
    
    Fixes #6338
    
    * Added cross-checking with Java results

---
+++ src/SonarLint.Secrets.DotNet/Rules/Matching/ConditionalMatcher.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Copyright (C) 2018-2021 SonarSource SA
+ * All rights reserved
+ * mailto:info AT sonarsource DOT com
+ */
+
+// Ported from ...\sonar-secrets-plugin\src\main\java\com\sonarsource\secrets\rules\matching\ConditionalMatcher.java
+
+using System;
+using System.Collections.Generic;
+
+namespace SonarLint.Secrets.DotNet.Rules.Matching
+{
+    internal class ConditionalMatcher : SecretsMatcher
+    {
+        private static readonly List<Match> EmptyList = new List<Match>();
+
+        private readonly Predicate<string> predicate;
+        private readonly SecretsMatcher ifTrue;
+
+        public ConditionalMatcher(Predicate<string> predicate, SecretsMatcher ifTrue)
+        {
+            this.predicate = predicate;
+            this.ifTrue = ifTrue;
+        }
+
+        public List<Match> findIn(string content)
+        {
+            return predicate(content) ? ifTrue.findIn(content) : EmptyList;
+        }
+    }
+}
