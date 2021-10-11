ccb00900ed999d417adcad88c70378876a66eb57
duncanp-sonar <duncan.pocklington@sonarsource.com>
2021-10-11 17:01:56 +0100

    [.NET] Added S6338 - Azure Storage Account Keys (#42)
    * [.NET] Added S6338 - Azure Storage Account Keys
    
    Fixes #6338
    
    * Added cross-checking with Java results

---
+++ src/SonarLint.Secrets.DotNet.UnitTests/Rules/AzureStorageAccountKeyRuleTest.cs
@@ -0,0 +1,89 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SonarLint.Secrets.DotNet.Rules;
+using SonarLint.VisualStudio.Integration.UnitTests;
+using System.Linq;
+using static SonarLint.Secrets.DotNet.UnitTests.TestUtils;
+
+namespace SonarLint.Secrets.DotNet.UnitTests.Rules
+{
+    [TestClass]
+    public class AzureStorageAccountKeyRuleTest
+    {
+        [TestMethod]
+        public void MefCtor_CheckIsExported()
+        {
+            MefTestHelpers.CheckTypeCanBeImported<AzureStorageAccountKeyRule, ISecretDetector>(null, null);
+        }
+
+        [TestMethod]
+        public void testRuleProperties()
+        {
+            var testSubject = new AzureStorageAccountKeyRule();
+
+            testSubject.RuleKey.Should().Be("S6338");
+            testSubject.Name.Should().Be("Azure Storage Account Keys should not be disclosed");
+            testSubject.Message.Should().Be("Make sure this Azure Storage Account Key is not disclosed.");
+        }
+
+        [TestMethod]
+        public void testRuleFirstRegexPositive()
+        {
+            var testSubject = new AzureStorageAccountKeyRule();
+
+            var input = "(async function main() {\n" +
+              "  const account = process.env.ACCOUNT_NAME || \"accountname\";\n" +
+              "  const accountKey = process.env.ACCOUNT_KEY || \"4dVw+l0W8My+FwuZ08dWXn+gHxcmBtS7esLAQSrm6/Om3jeyUKKGMkfAh38kWZlItThQYsg31v23A0w/uVP4pg==\";\n" +
+              "  const sharedKeyCredential = new StorageSharedKeyCredential(account, accountKey);\n" +
+              "  const blobServiceClient = new BlobServiceClient(\n" +
+              "    `https://${account}.blob.core.windows.net`,\n" +
+              "    sharedKeyCredential\n" +
+              "  );\n" +
+              "}";
+
+            var secrets = testSubject.Find(input);
+
+            secrets.Count().Should().Be(1);
+            CheckExpectedSecretFound(input, "4dVw+l0W8My+FwuZ08dWXn+gHxcmBtS7esLAQSrm6/Om3jeyUKKGMkfAh38kWZlItThQYsg31v23A0w/uVP4pg==", secrets.First());
+            CrossCheckWithJavaResult(input, new(3, 49, 3, 137), secrets.First());
+        }
+
+        [TestMethod]
+        public void testRuleSecondRegexPositive()
+        {
+            var testSubject = new AzureStorageAccountKeyRule();
+
+            var input = "const connStr = \"DefaultEndpointsProtocol=https;AccountName=testaccountname;AccountKey=4dVw+l0W8My+FwuZ08dWXn+gHxcmBtS7esLAQSrm6/Om3jeyUKKGMkfAh38kWZlItThQYsg31v23A0w/uVP4pg==\";";
+
+            var secrets = testSubject.Find(input);
+
+            secrets.Count().Should().Be(1);
+            CheckExpectedSecretFound(input, "4dVw+l0W8My+FwuZ08dWXn+gHxcmBtS7esLAQSrm6/Om3jeyUKKGMkfAh38kWZlItThQYsg31v23A0w/uVP4pg==", secrets.First());
+            CrossCheckWithJavaResult(input, new(1, 87, 1, 175), secrets.First());
+        }
+
+        [TestMethod]
+        public void testRuleSecondRegexPositiveEvenWhenCoreWindowsNetStringPresent()
+        {
+            var testSubject = new AzureStorageAccountKeyRule();
+
+            var input = "const connStr = \"DefaultEndpointsProtocol=https;AccountName=testaccountname;AccountKey=4dVw+l0W8My+FwuZ08dWXn+gHxcmBtS7esLAQSrm6/Om3jeyUKKGMkfAh38kWZlItThQYsg31v23A0w/uVP4pg==;EndpointSuffix=core.windows.net\";";
+
+            var secrets = testSubject.Find(input);
+
+            secrets.Count().Should().Be(1);
+            CheckExpectedSecretFound(input, "4dVw+l0W8My+FwuZ08dWXn+gHxcmBtS7esLAQSrm6/Om3jeyUKKGMkfAh38kWZlItThQYsg31v23A0w/uVP4pg==", secrets.First());
+            CrossCheckWithJavaResult(input, new(1, 87, 1, 175), secrets.First());
+        }
+
+        [TestMethod]
+        public void testRuleRegexNegative()
+        {
+            var testSubject = new AzureStorageAccountKeyRule();
+
+            var secrets = testSubject.Find("AccountKey=BtS7esLAQSrm6/Om3jeyUKKGMkfAh38kWZlItThQYsg31v23A0w/uVP4pg==\";");
+
+            secrets.Should().BeEmpty();
+        }
+    }
+}
