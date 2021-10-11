9910bb90ea20082961772eeb4223072514f04caf
duncanp-sonar <duncan.pocklington@sonarsource.com>
2021-10-11 22:46:37 +0100

    [.NET] Implement S6336 - Alibaba Cloud AccessKeys (#46)
    * [.NET] Implement S6336 - Alibaba Cloud AccessKeys
    
    Fixes SLVS #2677

---
+++ src/SonarLint.Secrets.DotNet.UnitTests/Rules/AlibabaCloudAccessKeySecretsRuleTest.cs
@@ -0,0 +1,100 @@
+﻿/*
+ * Copyright (C) 2018-2021 SonarSource SA
+ * All rights reserved
+ * mailto:info AT sonarsource DOT com
+ */
+
+// Ported from ...\sonar-secrets-plugin\src\test\java\com\sonarsource\secrets\rules\AlibabaCloudAccessKeySecretsRuleTest.java
+
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SonarLint.Secrets.DotNet.Rules;
+using SonarLint.VisualStudio.Integration.UnitTests;
+using System.Linq;
+using static SonarLint.Secrets.DotNet.UnitTests.JavaTestFileUtils;
+using static SonarLint.Secrets.DotNet.UnitTests.TestUtils;
+
+namespace SonarLint.Secrets.DotNet.UnitTests.Rules
+{
+    [TestClass]
+    public class AlibabaCloudAccessKeySecretsRuleTest
+    {
+        [TestMethod]
+        public void MefCtor_CheckIsExported()
+        {
+            MefTestHelpers.CheckTypeCanBeImported<AlibabaCloudAccessKeySecretsRule, ISecretDetector>(null, null);
+        }
+
+        [TestMethod]
+        public void testRuleProperties()
+        {
+            var testSubject = new AlibabaCloudAccessKeySecretsRule();
+
+            testSubject.RuleKey.Should().Be("S6336");
+            testSubject.Name.Should().Be("Alibaba Cloud AccessKeys should not be disclosed");
+            testSubject.Message.Should().Be("Make sure this Alibaba Cloud Access Key Secret is not disclosed.");
+        }
+
+        [TestMethod]
+        public void testRuleRegex1Positive()
+        {
+            var testSubject = new AlibabaCloudAccessKeySecretsRule();
+
+            var input = "String aliyunAccessKeySecret=\"KmkwlDrPBC68bgvZiNtrjonKIYmVT8\";";
+
+            var secrets = testSubject.Find(input);
+
+            secrets.Count().Should().Be(1);
+            CheckExpectedSecretFound(input, "KmkwlDrPBC68bgvZiNtrjonKIYmVT8", secrets.First());
+            CrossCheckWithJavaResult(input, new(1, 30, 1, 60), secrets.First());
+        }
+
+        [TestMethod]
+        public void testRuleRegex2Positive()
+        {
+            var testSubject = new AlibabaCloudAccessKeySecretsRule();
+
+            var input = "static string AccessKeySecret = \"l0GdwcDYdJwB1VJ5pv0ormyTV9nhvW \";";
+
+            var secrets = testSubject.Find(input);
+
+            secrets.Count().Should().Be(1);
+            CheckExpectedSecretFound(input, "l0GdwcDYdJwB1VJ5pv0ormyTV9nhvW", secrets.First());
+            CrossCheckWithJavaResult(input, new(1, 33, 1, 63), secrets.First());
+        }
+
+        [TestMethod]
+        public void testRuleRegexNegative()
+        {
+            var testSubject = new AlibabaCloudAccessKeySecretsRule();
+
+            var secrets = testSubject.Find(readFileAndNormalize("src/test/files/google-cloud-account-key/GoogleCloudAccountNegative.json", UTF_8));
+
+            secrets.Should().BeEmpty();
+        }
+
+        [TestMethod]
+        public void testRuleRegexExamplePositive()
+        {
+            var testSubject = new AlibabaCloudAccessKeySecretsRule();
+
+            var input = "String aliyunAccessKeySecret=\"KmkwlDrPBC68bgvZiNtrjonKIYmVT8\";";
+
+            var secrets = testSubject.Find(input);
+
+            secrets.Count().Should().Be(1);
+            CheckExpectedSecretFound(input, "KmkwlDrPBC68bgvZiNtrjonKIYmVT8", secrets.First());
+            CrossCheckWithJavaResult(input, new(1, 30, 1, 60), secrets.First());
+        }
+
+        [TestMethod]
+        public void testRuleRegexExampleNegativeLowEntropy()
+        {
+            var testSubject = new AlibabaCloudAccessKeySecretsRule();
+
+            var secrets = testSubject.Find("String aliyunAccessKeySecret=\"100000000000000000000000000000\";");
+
+            secrets.Should().BeEmpty();
+        }
+    }
+}
