cfa911628813f97c99508a459e9e8a2e63898123
duncanp-sonar <duncan.pocklington@sonarsource.com>
2021-10-11 17:11:24 +0100

    [.NET] Added S6290 - AWS credentials (#43)
    * [.NET] Added S6290 - AWS credentials
    
    Fixes SLVS #2667

---
+++ src/SonarLint.Secrets.DotNet/Rules/AwsAccessKeyRule.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Copyright (C) 2018-2021 SonarSource SA
+ * All rights reserved
+ * mailto:info AT sonarsource DOT com
+ */
+
+// Ported from ...\sonar-secrets-plugin\src\main\java\com\sonarsource\secrets\rules\AwsAccessKeyRule.java
+
+using SonarLint.Secrets.DotNet.Rules.Matching;
+using System.ComponentModel.Composition;
+
+namespace SonarLint.Secrets.DotNet.Rules
+{
+    [Export(typeof(ISecretDetector))]
+    internal class AwsAccessKeyRule : AbstractAwsRule
+    {
+        private readonly EntropyChecker entropyChecker;
+
+        public AwsAccessKeyRule()
+
+              : base("Make sure this AWS Secret Access Key is not disclosed.",
+                new RegexMatcher("(?i)aws.{0,50}['\"`]([0-9a-z\\/+]{40})['\"`]"),
+                new RegexMatcher("(?i)\\b(?:AWS)?_?SECRET_?(?:ACCESS)?_?KEY\\b.{0,10}([0-9a-z\\/+]{40})"))
+        {
+            entropyChecker = new EntropyChecker();
+        }
+
+        protected override bool isProbablyFalsePositive(string matchedText)
+        {
+            return entropyChecker.hasLowEntropy(matchedText);
+        }
+    }
+}
