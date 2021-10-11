aa72b0616eb3c55622279208ef089cef6de8355d
duncanp-sonar <duncan.pocklington@sonarsource.com>
2021-10-11 22:50:55 +0100

    [.NET] Improve test checking (#47)
    * [.NET] Improve test checking

--- src/SonarLint.Secrets.DotNet.UnitTests/Rules/GoogleCloudAccountKeyRuleTest.cs
+++ src/SonarLint.Secrets.DotNet.UnitTests/Rules/GoogleCloudAccountKeyRuleTest.cs
@@ -11,8 +11,9 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SonarLint.Secrets.DotNet.Rules;
 using SonarLint.VisualStudio.Integration.UnitTests;
 using System.Linq;
-
 using static SonarLint.Secrets.DotNet.UnitTests.JavaTestFileUtils;
+using static SonarLint.Secrets.DotNet.UnitTests.TestUtils;
+
 namespace SonarLint.Secrets.DotNet.UnitTests.Rules
 {
     [TestClass]
@@ -39,12 +40,15 @@ namespace SonarLint.Secrets.DotNet.UnitTests.Rules
         {
             var testSubject = new GoogleCloudAccountKeyRule();
 
-            var secrets = testSubject.Find(readFileAndNormalize("src/test/files/google-cloud-account-key/GoogleCloudAccountPositive.json", UTF_8));
+            // Note: the expected value in the file contains backslashes, which need to be escaped in the following string variable
+            var expectedSecret = "-----BEGIN PRIVATE KEY-----\\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCqypYh1ms+SNYj\\n8o7F1a4v9JO2h4Rj6TO7sUaiBlmqERTnjMdiNmQiJB6zS+iVExh804vleIYHv75E\\n5BEZxeUTuA6s3PDs9fGhql6YFY8p7sVarbfg3X50trVvfz85XmvnNj/+dlWTu73g\\nAnsCiDmgdjmQXbZsAXaY0S3eZO2cgkG7btyG31+uqSOUrohscJkLIqEaI4p6rDWa\\nw9TgtFiGt8GlrRSAEXREmbHQeOvgfPdSHVCp0MuZ2XQPLH+zoydk5AxgMDEPPWOn\\ngMEmnPU41S9VT4CYcTNs4L+keoECNggRqYvNfGr6MdQmfOkwk6VU31DLKLNtzkKS\\n+9ucI9wVAgMBAAECggEAGa8If8MucnnilhpSZSbaoISPuBnOC3ZU3Mtue0woLL6P\\n3bhafYb/bPDwmvb13YE3JAJuHc6PnVe05chEWBNd/SanMlCxfHkJ4s1rl0RauKwb\\nCqVoKw00CJo3U7KXj+mGoYkTe5FREjvf9HF7DSjuPZa51l95YOmha569Xp3f67Kd\\n31olO+gqKvZJhgvDMjhSMormGRtucCwKigTvqT+jvw0NbajtyReD5AnslHroRI6h\\nawMASrCl2gp6iR5h98FP9fi5dtSnTyWQjoWZqHYov95h490t3ZDUAvfjhNIEBiaC\\n5PgG8NqiKRu7gGjZw66/vYox5V/tscCOtZ4M2/LCawKBgQDhF9ESmsCh3lMIwVpn\\nJA4xrKpA89Bxk7X11L5ORCXeI+hicwQpv3aaUDFhSw039joSyBUsu6+cnR/2rqor\\nhHJ4aRfNByFjWallp/dFoEjTAVLLX3YYWGjeFcnls8yIjC273TuztrC45+BTwPQ/\\npi49cHVib1upZGogKd765cFovwKBgQDCPga9afKA5fmYDpV9nNEZkDyJqHx84OxV\\ng2iQrBT91EnfQ4jWwDUbPgoV/PxNFGIJriO+h80jpOFfdr8BR8tmagGkNW35PHVW\\nA23kXA77WBDlwLOvYJMayLiS+6qPD9GKXUOs68330nKhV7gCw9ptP6dj8nm4tARC\\nqtzWEAW8KwKBgQDc3zX0f43OSA5KZRCEbMjQzZEyswwyprLCSsOCoTRXSfzZveqz\\n3IBQ46fQxIhwBVju3Q6KGpEzqKqYsMOieBCrPtRwBzMn/e9PJQZqd+F9y6qmjUGg\\nmgAtDExU7Z0h0AuAcJIIwpeemUlyZcBGRJbTCurkcEkNas8ISI3YvGKQmQKBgGuT\\nfpgYvT22IG2eZhyTZRsLTvvOXGOtDjat+JNnOpj5oZANDxQgj0jvKxbSbckiqMlD\\nsHgtLee3wTnlwhMrd/LYcuBG1wlZ+oIQlCQM+B8rvu//sYRHsDD9SXvd9bAVyq8e\\nyARU9FV0MBg7RKYDYk1vG323JMKusE1b48KKTSLRAoGAGGILCOCFKlBt43u5bj9j\\n6xDEOBn/LG8zyuGOFqIkE3iHbLuMaFpXpKT4dsx51gSrN67Zsgmj/Zf2OxU7vTfF\\nHP+SrqniXDOSsmjHWXs+V7enSC/7e/Lwaw8rNI42hizLMzNNqPx+5Vd/vBIN6z0U\\ncU6a4WWC772nb+mLIODMlx0=\\n-----END PRIVATE KEY-----\\n";
+
+            var input = readFileAndNormalize("src/test/files/google-cloud-account-key/GoogleCloudAccountPositive.json", UTF_8);
+            var secrets = testSubject.Find(input);
 
             secrets.Count().Should().Be(1);
-            secrets.First().StartIndex.Should().Be(153);
-            // The Java test checks for the tuple(5, 18, 5, 1750) (start.line, start.lineOffset, end.line, end.lineOffset)
-            secrets.First().Length.Should().Be(1750 - 18);
+            CheckExpectedSecretFound(input, expectedSecret, secrets.First());
+            CrossCheckWithJavaResult(input, new(5, 18, 5, 1750), secrets.First());
         }
 
         [TestMethod]
