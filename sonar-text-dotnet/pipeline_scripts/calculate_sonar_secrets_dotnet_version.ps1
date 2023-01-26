608ba282bcef1f06096041dd6c7f10446e9b6d6e
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2023-01-26 17:38:25 +0100

    Compute project version script

--- sonar-text-dotnet/pipeline_scripts/calculate_sonar_secrets_dotnet_version.ps1
+++ sonar-text-dotnet/pipeline_scripts/calculate_sonar_secrets_dotnet_version.ps1
@@ -1,14 +1,8 @@
-# Calculate the file path
-$BuildConfiguration="Release"
-$Major="0"
-$Minor="0"
+
 $versionFilePath = "$env:CIRRUS_WORKING_DIR\sonar-text-dotnet\src\SonarLint.Secrets.DotNet\SonarLint.Secrets.DotNet.csproj"
 Write-Host "Reading the Sonar project version from '${versionFilePath}' ..."
 # Read the version from the file
 [xml]$versionProps = Get-Content "$versionFilePath"
-$sonarProjectVersion = $versionProps.Project.PropertyGroup.AssemblyVersion
-
-Write-host $version
-Write-Host "Sonar project version is '${sonarProjectVersion}'"
-# Set the variable to it can be used by other tasks
-Write-Host $sonarProjectVersion
+$sonarProjectVersion = $versionProps.Project.PropertyGroup.Version
+[Environment]::SetEnvironmentVariable('PROJECT_VERSION', $sonarProjectVersion, 'Machine') # Persist this variable on machine level
+Write-Host "Sonar project version is $env:PROJECT_VERSION"
