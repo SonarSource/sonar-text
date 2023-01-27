0321973352c4d24805d4e2255dcc604423db1732
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2023-01-27 16:51:16 +0100

    analysis begin step

--- sonar-text-dotnet/pipeline_scripts/build_and_restore_solution.ps1
+++ sonar-text-dotnet/pipeline_scripts/build_and_restore_solution.ps1
@@ -1,6 +1,7 @@
 $buildConfiguration="Release"
 
 cd $env:PROJECT_DIR
+
 Write-Host Restoring $env:SOLUTION_DIR
 nuget restore -LockedMode -Source "https://pkgs.dev.azure.com/sonarsource/399fb241-ecc7-4802-8697-dcdd01fbb832/_packaging/slvs_input/nuget/v3/index.json"  # nuget version is 6.3.1 https://github.com/SonarSource/re-ci-images/blob/master/ec2-images/build-base-windows-dotnet.pkr.hcl#L39
 
