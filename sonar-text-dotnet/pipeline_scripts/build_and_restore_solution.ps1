042fe942cd249925b4cf2bccdfe42599797ec1b9
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2023-01-27 15:52:33 +0100

    Add extra msbuild vars to build

--- sonar-text-dotnet/pipeline_scripts/build_and_restore_solution.ps1
+++ sonar-text-dotnet/pipeline_scripts/build_and_restore_solution.ps1
@@ -5,9 +5,9 @@ Write-Host Restoring $env:SOLUTION_DIR
 nuget restore -LockedMode -Source "https://pkgs.dev.azure.com/sonarsource/399fb241-ecc7-4802-8697-dcdd01fbb832/_packaging/slvs_input/nuget/v3/index.json"  # nuget version is 6.3.1 https://github.com/SonarSource/re-ci-images/blob/master/ec2-images/build-base-windows-dotnet.pkr.hcl#L39
 
 $signArtifacts=$false
-if(($env:CIRRUS_BRANCH -eq "master" ) -or ($env:CIRRUS_BRANCH.StartsWith("branch-"))
+if(($env:CIRRUS_BRANCH -eq "master" ) -or ($env:CIRRUS_BRANCH.StartsWith("branch-")))
 {
-    $signArtifacts=true
+    $signArtifacts=$true
     Write-Host "signArtifacts variable was set to {$signArtifacts}".
 }
 
