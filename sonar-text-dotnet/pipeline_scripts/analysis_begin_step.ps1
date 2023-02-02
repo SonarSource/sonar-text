8d4507c99775c0564c1a82d399020876e0e3ab53
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2023-02-02 15:16:44 +0100

    differentiate analysis script between master and pr

--- sonar-text-dotnet/pipeline_scripts/analysis_begin_step.ps1
+++ sonar-text-dotnet/pipeline_scripts/analysis_begin_step.ps1
@@ -1,14 +1,27 @@
 Set-Location $env:PROJECT_DIR
 
-Write-Host "Execute analysis begin step"
 
-SonarScanner.MSBuild.exe begin `
-	/k:$env:PROJECT_NAME `
-	/n:$env:PROJECT_NAME `
-	/v:$env:CIRRUS_CHANGE_IN_REPO `
-	/d:sonar.host.url=$env:SONAR_HOST_URL `
-	/d:sonar.login=$env:SONAR_TOKEN `
-	/d:sonar.pullrequest.branch=$env:CIRRUS_BRANCH `
-	/d:sonar.pullrequest.key=$env:CIRRUS_PR `
-	/d:sonar.pullrequest.base=$env:CIRRUS_BASE_BRANCH
+if ($env:CIRRUS_BRANCH -eq "master") {
+    Write-Host "Execute analysis begin step for master branch"  
+    SonarScanner.MSBuild.exe begin `
+        /k:$env:PROJECT_NAME `
+        /n:$env:PROJECT_NAME `
+        /v:$env:CIRRUS_CHANGE_IN_REPO `
+        /d:sonar.host.url=$env:SONAR_HOST_URL `
+        /d:sonar.login=$env:SONAR_TOKEN `
+ 
+}
+else {
+    
+    Write-Host "Execute analysis begin step on branch $env:CIRRUS_BRANCH"
 
+    SonarScanner.MSBuild.exe begin `
+        /k:$env:PROJECT_NAME `
+        /n:$env:PROJECT_NAME `
+        /v:$env:CIRRUS_CHANGE_IN_REPO `
+        /d:sonar.host.url=$env:SONAR_HOST_URL `
+        /d:sonar.login=$env:SONAR_TOKEN `
+        /d:sonar.pullrequest.branch=$env:CIRRUS_BRANCH `
+        /d:sonar.pullrequest.key=$env:CIRRUS_PR `
+        /d:sonar.pullrequest.base=$env:CIRRUS_BASE_BRANCH
+}
