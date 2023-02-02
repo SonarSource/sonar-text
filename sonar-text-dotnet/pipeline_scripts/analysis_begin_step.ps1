0a868b7eb8220bf3e4978fcde11d5d886bf3dfd3
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2023-02-02 15:18:53 +0100

    fix codings style

--- sonar-text-dotnet/pipeline_scripts/analysis_begin_step.ps1
+++ sonar-text-dotnet/pipeline_scripts/analysis_begin_step.ps1
@@ -1,18 +1,16 @@
 Set-Location $env:PROJECT_DIR
 
-
 if ($env:CIRRUS_BRANCH -eq "master") {
-    Write-Host "Execute analysis begin step for master branch"  
+    Write-Host "Execute analysis begin step for master branch" 
+    
     SonarScanner.MSBuild.exe begin `
         /k:$env:PROJECT_NAME `
         /n:$env:PROJECT_NAME `
         /v:$env:CIRRUS_CHANGE_IN_REPO `
         /d:sonar.host.url=$env:SONAR_HOST_URL `
         /d:sonar.login=$env:SONAR_TOKEN `
- 
 }
 else {
-    
     Write-Host "Execute analysis begin step on branch $env:CIRRUS_BRANCH"
 
     SonarScanner.MSBuild.exe begin `
