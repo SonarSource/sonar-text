23400ae15f570bba9b6d4a85115c4637c1673e48
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2023-02-03 07:31:29 +0100

    check if CIRRUS_PR is empty and not if the branch is named master

--- sonar-text-dotnet/src/SonarLint.Secrets.DotNet/Rules/Matching/analysis_begin_step.ps1
+++ sonar-text-dotnet/src/SonarLint.Secrets.DotNet/Rules/Matching/analysis_begin_step.ps1
@@ -1,6 +1,16 @@
 Set-Location $env:PROJECT_DIR
 
-if ($env:CIRRUS_PR -ne "") {
+if ($env:CIRRUS_BRANCH -eq "master") {
+    Write-Host "Execute analysis begin step for master branch" 
+    
+    SonarScanner.MSBuild.exe begin `
+        /k:$env:PROJECT_NAME `
+        /n:$env:PROJECT_NAME `
+        /v:$env:CIRRUS_CHANGE_IN_REPO `
+        /d:sonar.host.url=$env:SONAR_HOST_URL `
+        /d:sonar.login=$env:SONAR_TOKEN `
+}
+else {
     Write-Host "Execute analysis begin step on branch $env:CIRRUS_BRANCH"
 
     SonarScanner.MSBuild.exe begin `
@@ -12,14 +22,4 @@ if ($env:CIRRUS_PR -ne "") {
         /d:sonar.pullrequest.branch=$env:CIRRUS_BRANCH `
         /d:sonar.pullrequest.key=$env:CIRRUS_PR `
         /d:sonar.pullrequest.base=$env:CIRRUS_BASE_BRANCH
-}
-else {
-    Write-Host "Execute analysis begin step for master branch"
-
-    SonarScanner.MSBuild.exe begin `
-        /k:$env:PROJECT_NAME `
-        /n:$env:PROJECT_NAME `
-        /v:$env:CIRRUS_CHANGE_IN_REPO `
-        /d:sonar.host.url=$env:SONAR_HOST_URL `
-        /d:sonar.login=$env:SONAR_TOKEN `
 }
