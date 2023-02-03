381e3a84ca9db38872bc8cdea2420746762e270b
mary-georgiou-sonarsource <mary.georgiou@sonarsource.com>
2023-02-03 14:18:41 +0100

    remove version from begin step on PRs

--- sonar-text-dotnet/pipeline_scripts/analysis_begin_step.ps1
+++ sonar-text-dotnet/pipeline_scripts/analysis_begin_step.ps1
@@ -16,7 +16,6 @@ else {
     SonarScanner.MSBuild.exe begin `
         /k:$env:PROJECT_NAME `
         /n:$env:PROJECT_NAME `
-        /v:$env:CIRRUS_CHANGE_IN_REPO `
         /d:sonar.host.url=$env:SONAR_HOST_URL `
         /d:sonar.login=$env:SONAR_TOKEN `
         /d:sonar.pullrequest.branch=$env:CIRRUS_BRANCH `
