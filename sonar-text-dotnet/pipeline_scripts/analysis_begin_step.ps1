8c0c4657db1b5abd7f7b74c47b811767887e5f1e
Costin Zaharia <56015273+costin-zaharia-sonarsource@users.noreply.github.com>
2023-02-08 18:13:38 +0100

    Add coverage support (#101)

--- sonar-text-dotnet/pipeline_scripts/analysis_begin_step.ps1
+++ sonar-text-dotnet/pipeline_scripts/analysis_begin_step.ps1
@@ -9,6 +9,7 @@ if ([string]::IsNullOrEmpty($env:CIRRUS_PR)) {
         /v:$env:PROJECT_VERSION `
         /d:sonar.host.url=$env:SONAR_HOST_URL `
         /d:sonar.login=$env:SONAR_TOKEN `
+        /d:sonar.cs.opencover.reportsPaths="**/coverage.xml" 
 }
 else {
     Write-Host "Execute analysis begin step on branch $env:CIRRUS_BRANCH"
@@ -20,7 +21,8 @@ else {
         /d:sonar.login=$env:SONAR_TOKEN `
         /d:sonar.pullrequest.branch=$env:CIRRUS_BRANCH `
         /d:sonar.pullrequest.key=$env:CIRRUS_PR `
-        /d:sonar.pullrequest.base=$env:CIRRUS_BASE_BRANCH
+        /d:sonar.pullrequest.base=$env:CIRRUS_BASE_BRANCH `
+        /d:sonar.cs.opencover.reportsPaths="**/coverage.xml" 
 }
 
 if(!$?) {
