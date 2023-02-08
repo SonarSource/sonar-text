8e1788b7f78f22184a62dc7183209ad8931ef540
Costin Zaharia <56015273+costin-zaharia-sonarsource@users.noreply.github.com>
2023-02-08 15:04:42 +0100

    Publish nuget package (#100)

---
+++ sonar-text-dotnet/pipeline_scripts/promote.ps1
@@ -0,0 +1,16 @@
+Set-Location "$env:CIRRUS_WORKING_DIR\\sonar-text-dotnet"
+
+Write-Host "Setting up artifactory authentication for promotion"
+jf config add "repox" --url https://repox.jfrog.io --access-token $env:ARTIFACTORY_PROMOTE_TOKEN --overwrite
+jf rt ping # Check if the artifactory configuration is successfull
+
+$isMasterOrBranchBuild  = "$env:CIRRUS_BRANCH" -eq "master" -or "$env:CIRRUS_BRANCH".startsWith("branch-")
+$sourceRepo = "sonarsource-nuget-qa"
+$destinationRepo = if ($isMasterOrBranchBuild) { "sonarsource-nuget-public" } else { "sonarsource-nuget-dev" }
+
+Write-Host "Promoting the NuGet package from $sourceRepo to $destinationRepo"
+jf rt build-promote $env:ARTIFACTORY_BUILD_NAME $env:BUILD_NUMBER $destinationRepo --source-repo $sourceRepo
+if(!$?) {
+  Write-Host "sonar-text-dotnet promote step failed."
+  Exit $LASTEXITCODE
+}
