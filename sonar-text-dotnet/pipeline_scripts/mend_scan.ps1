a132f8aa81a1fe0aa754a78a315ecc0ce3c19b31
Čaba Šagi <csaba.sagi@sonarsource.com>
2023-02-09 14:33:24 +0100

    Change java home env variable

--- sonar-text-dotnet/pipeline_scripts/mend_scan.ps1
+++ sonar-text-dotnet/pipeline_scripts/mend_scan.ps1
@@ -40,7 +40,7 @@ for ($num = 1 ; $num -le $NUM_RETRIES ; $num++)
 }
 
 Write-Host "Validating Mend agent jar signature..."
-& "$env:JAVA_HOME_11_X64\bin\jarsigner.exe" -verify -strict -verbose $MendAgentPath
+& "$env:JAVA_HOME\bin\jarsigner.exe" -verify -strict -verbose $MendAgentPath
 if (-Not $?) # if result is "jar is unsigned" exit code is false, otherwise it's true.
 {
   Write-Host "wss-unified-agent.jar signature verification failed."
@@ -70,4 +70,4 @@ if (-Not (Get-Content $shaPath).split(" ")[0] -eq (Get-FileHash $MendAgentPath).
 $env:WS_PROJECTNAME = "$env:PROJECT_NAME $(Get-Version)"
 
 Write-Host "Running the Mend unified agent for $env:WS_PROJECTNAME..."
-#& "$env:JAVA_HOME_11_X64\bin\java.exe" -jar $MendAgentPath -c "$PSScriptRoot\wss-unified-agent.config"
+#& "$env:JAVA_HOME\bin\java.exe" -jar $MendAgentPath -c "$PSScriptRoot\wss-unified-agent.config"
