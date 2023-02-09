cc91e86da4675f77989042e32c5973b46d258fa4
Costin Zaharia <56015273+costin-zaharia-sonarsource@users.noreply.github.com>
2023-02-09 09:15:55 +0100

    Update the project version before using it (#102)

--- sonar-text-dotnet/pipeline_scripts/analysis_begin_step.ps1
+++ sonar-text-dotnet/pipeline_scripts/analysis_begin_step.ps1
@@ -1,3 +1,11 @@
+$versionFilePath = "$env:PROJECT_DIR\Directory.Build.props"
+Write-Host "Reading the Sonar project version from '${versionFilePath}' ..."
+
+# Read the version from the file
+[xml]$versionProps = Get-Content "$versionFilePath"
+$sonarProjectVersion = $versionProps.Project.PropertyGroup.Version
+Write-Host "Version: ${sonarProjectVersion}"
+
 Set-Location $env:PROJECT_DIR
 
 if ([string]::IsNullOrEmpty($env:CIRRUS_PR)) {
@@ -6,7 +14,7 @@ if ([string]::IsNullOrEmpty($env:CIRRUS_PR)) {
     SonarScanner.MSBuild.exe begin `
         /k:$env:PROJECT_NAME `
         /n:$env:PROJECT_NAME `
-        /v:$env:PROJECT_VERSION `
+        /v:$sonarProjectVersion `
         /d:sonar.host.url=$env:SONAR_HOST_URL `
         /d:sonar.login=$env:SONAR_TOKEN `
         /d:sonar.cs.opencover.reportsPaths="**/coverage.xml" 
@@ -17,6 +25,7 @@ else {
     SonarScanner.MSBuild.exe begin `
         /k:$env:PROJECT_NAME `
         /n:$env:PROJECT_NAME `
+        /v:$sonarProjectVersion `
         /d:sonar.host.url=$env:SONAR_HOST_URL `
         /d:sonar.login=$env:SONAR_TOKEN `
         /d:sonar.pullrequest.branch=$env:CIRRUS_BRANCH `
