46575ba69fc5a644002cf0b62a8115f074af8d4b
Jonas Wielage <jonas.wielage@sonarsource.com>
2023-05-19 10:44:26 +0200

    SONARTEXT-12 Validate the configuration file using the json-schema

---
+++ sonar-text-plugin/src/main/java/org/sonar/plugins/secrets/configuration/SchemaValidator.java
@@ -0,0 +1,78 @@
+/*
+ * SonarQube Text Plugin
+ * Copyright (C) 2021-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.plugins.secrets.configuration;
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.networknt.schema.JsonSchema;
+import com.networknt.schema.JsonSchemaFactory;
+import com.networknt.schema.SpecVersion;
+import com.networknt.schema.ValidationMessage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InvalidObjectException;
+import java.net.URL;
+import java.util.Set;
+
+public class SchemaValidator {
+
+  private static final String FILE_LOCATION = "/org/sonar/plugins/secrets/configuration/";
+  private static final String VALIDATION_SCHEMA_FILE = "specification-json-schema.json";
+  private static final Set<String> SPECIFICATION_FILES = Set.of("minSpec.yaml");
+
+  private static final ObjectMapper MAPPER = new ObjectMapper(new YAMLFactory());
+
+  private static final JsonSchema VALIDATION_SCHEMA = initializeValidationSchema();
+
+  private SchemaValidator() {
+
+  }
+
+  private static JsonSchema initializeValidationSchema() {
+    JsonSchemaFactory schemaFactory = JsonSchemaFactory
+      .builder(JsonSchemaFactory.getInstance(SpecVersion.VersionFlag.V202012))
+      .objectMapper(MAPPER)
+      .build();
+    InputStream validationSchema = SchemaValidator.class.getResourceAsStream(FILE_LOCATION + VALIDATION_SCHEMA_FILE);
+    return schemaFactory.getSchema(validationSchema);
+  }
+
+  public static void validateConfigurationFiles() {
+    for (String configurationFile : SPECIFICATION_FILES) {
+      URL configuration = SchemaValidator.class.getResource(FILE_LOCATION + configurationFile);
+      validate(configuration);
+    }
+  }
+
+  public static void validate(URL configurationLocation) {
+    try {
+      Set<ValidationMessage> validate = VALIDATION_SCHEMA.validate(MAPPER.readTree(configurationLocation));
+      if (!validate.isEmpty()) {
+        String filePath = configurationLocation.getPath();
+        String fileName = filePath.substring(filePath.lastIndexOf('/') + 1);
+        String errorMessage = String.format("Specification file \"%s\" failed the schema validation", fileName);
+        throw new InvalidObjectException(errorMessage);
+      }
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+}
---
+++ sonar-text-plugin/src/test/java/org/sonar/plugins/secrets/configuration/SchemaValidatorTest.java
@@ -0,0 +1,60 @@
+/*
+ * SonarQube Text Plugin
+ * Copyright (C) 2021-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.plugins.secrets.configuration;
+
+import java.net.URL;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+class SchemaValidatorTest {
+
+  @Test
+  void specificationFilesAreValid() {
+    assertThatNoException().isThrownBy(SchemaValidator::validateConfigurationFiles);
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"validMinSpec.yaml", "validReferenceSpec.yaml"})
+  void testSpecificationFilesAreValid(String specificationFileName) {
+    URL specificationUrl = Thread.currentThread().getContextClassLoader().getResource("secretsConfiguration/" + specificationFileName);
+
+    assertThatNoException().isThrownBy(() -> SchemaValidator.validate(specificationUrl));
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {"invalidEmptySpec.yaml", "invalidSpecMissingRequiredField.yaml", "invalidSpecWithUnexpectedField.yaml",
+    "invalidSpecWithWrongType.yaml"})
+  void testSpecificationFilesAreInValid(String specificationFileName) {
+    URL specificationUrl = Thread.currentThread().getContextClassLoader().getResource("secretsConfiguration/" + specificationFileName);
+
+    assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> SchemaValidator.validate(specificationUrl));
+  }
+
+  @Test
+  void testSpecificationFilesAreInValid() {
+    URL specificationUrl = Thread.currentThread().getContextClassLoader().getResource("doesNotExist.yaml");
+
+    assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> SchemaValidator.validate(specificationUrl));
+  }
+}
