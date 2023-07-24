c630f1dc7c1182a3dea00e78e2ae5f9525d4a1f7
Jonas Wielage <127737854+jonas-wielage-sonarsource@users.noreply.github.com>
2023-07-24 09:41:05 +0200

    SONARTEXT-32 Add Pre-Filter to detection logic; Add additional integration tests (#163)

---
+++ its/ruling/src/test/resources/sources/secrets/S6652/settings.py
@@ -0,0 +1,3 @@
+# See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'r&lvybzry1*k+qq)=x-!=0yd5l5#1gxzk!82@ru25*ntos3_9^'
