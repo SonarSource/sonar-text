4818bdbf72c6da1f9f383a1cfed9ede2f291606b
Loris S <91723853+loris-s-sonarsource@users.noreply.github.com>
2023-08-31 11:05:43 +0200

    APPSEC-1007 Create S6732: Stripe API Keys should not be disclosed (#215)

--- sonar-text-plugin/src/main/java/org/sonar/plugins/secrets/SecretsRulesDefinition.java
+++ sonar-text-plugin/src/main/java/org/sonar/plugins/secrets/SecretsRulesDefinition.java
@@ -59,6 +59,7 @@ import org.sonar.plugins.secrets.checks.SlackWebhookURLUniqueNameCheck;
 import org.sonar.plugins.secrets.checks.SonarQubeCheck;
 import org.sonar.plugins.secrets.checks.SpotifyCheck;
 import org.sonar.plugins.secrets.checks.StripeWebhookSecretCheck;
+import org.sonar.plugins.secrets.checks.StripeWebhookSecretUniqueNameCheck;
 import org.sonar.plugins.secrets.checks.TelegramCheck;
 import org.sonar.plugins.secrets.checks.WeChatCheck;
 import org.sonar.plugins.secrets.checks.ZapierWebhookUrlCheck;
@@ -116,6 +117,7 @@ public class SecretsRulesDefinition extends CommonRulesDefinition {
       SonarQubeCheck.class,
       SpotifyCheck.class,
       StripeWebhookSecretCheck.class,
+      StripeWebhookSecretUniqueNameCheck.class,
       TelegramCheck.class,
       WeChatCheck.class,
       ZapierWebhookUrlCheck.class);
