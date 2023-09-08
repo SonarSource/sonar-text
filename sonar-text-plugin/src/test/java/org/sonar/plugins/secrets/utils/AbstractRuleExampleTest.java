88513da13d031f181d8715c038243fd863b15d1b
Jonas Wielage <127737854+jonas-wielage-sonarsource@users.noreply.github.com>
2023-09-08 11:44:13 +0200

    SONARTEXT-76 Improve messages for failing tests (#230)

--- sonar-text-plugin/src/test/java/org/sonar/plugins/secrets/utils/AbstractRuleExampleTest.java
+++ sonar-text-plugin/src/test/java/org/sonar/plugins/secrets/utils/AbstractRuleExampleTest.java
@@ -26,11 +26,12 @@ import java.util.List;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.api.function.Executable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.sonar.api.batch.fs.TextRange;
 import org.sonar.api.batch.sensor.internal.SensorContextTester;
 import org.sonar.api.batch.sensor.issue.Issue;
@@ -50,6 +51,8 @@ import static org.sonar.plugins.common.TestUtils.inputFile;
 import static org.sonar.plugins.common.TestUtils.sensorContext;
 
 public abstract class AbstractRuleExampleTest {
+
+  private static final Logger LOG = LoggerFactory.getLogger(AbstractRuleExampleTest.class);
   private static SpecificationLoader specificationLoader;
   private final Check check;
 
@@ -85,14 +88,16 @@ public abstract class AbstractRuleExampleTest {
       if (ruleExample.isContainsSecret()) {
         List<TextRange> expectedRanges = calculatePossibleRanges(ruleExample, inputFileContext);
 
-        assertThat(issues).isNotEmpty();
+        assertThat(issues).withFailMessage(() -> failingMessage(issues, rule, ruleExample)).isNotEmpty();
         assertThat(issues).anyMatch(s -> asString(s).contains(rule.getMetadata().getMessage()));
         assertThat(issues).anyMatch(s -> asString(s).contains(rule.getRspecKey()));
         // Since there may be multiple occurrences of `ruleExample.match` in `ruleExample.text`, we check if
         // any of them matches the actual range.
-        assertThat(issues).map(i -> i.primaryLocation().textRange()).containsAnyElementsOf(expectedRanges);
+        assertThat(expectedRanges).withFailMessage(() -> failingMessage(issues, rule, ruleExample)).isNotEmpty();
+        assertThat(issues).map(i -> i.primaryLocation().textRange())
+          .containsAnyElementsOf(expectedRanges);
       } else {
-        assertThat(issues).isEmpty();
+        assertThat(issues).withFailMessage(() -> failingMessage(issues, rule, ruleExample)).isEmpty();
       }
     };
   }
@@ -110,6 +115,59 @@ public abstract class AbstractRuleExampleTest {
     String positiveOrNegative = example.isContainsSecret() ? "" : " not";
     int indexOfExampleInRule = rule.getExamples().indexOf(example) + 1;
     return String.format("%s example %d: Should%s find issue", rule.getId(), indexOfExampleInRule, positiveOrNegative);
+  }
+
+  private String failingMessage(Collection<Issue> issues, Rule rule, RuleExample ruleExample) {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Test case \"");
+    sb.append(displayName(rule, ruleExample));
+    if (issues.isEmpty()) {
+      sb.append("\" failed as it didn't match on any content");
+      return sb.toString();
+    }
+
+    sb.append("\" failed as it matched on the following content:");
+    sb.append(System.lineSeparator());
+    for (Issue issue : issues) {
+      sb.append("- \"");
+      try {
+        sb.append(retrieveMatchedContent(issue, ruleExample.getText()));
+      } catch (NullPointerException | IndexOutOfBoundsException e) {
+        sb.append("<Could not calculate matched content cause of ");
+        sb.append(e.getClass().getSimpleName());
+        sb.append(">");
+        LOG.error("Exception calculating matched content, see test results below", e);
+      }
+      sb.append("\"");
+      sb.append(System.lineSeparator());
+    }
+
+    if (ruleExample.isContainsSecret()) {
+      sb.append("But the example should have matched on: \"");
+      sb.append(ruleExample.getMatch());
+      sb.append("\"");
+    } else {
+      sb.append("But the example doesn't expect a match");
+    }
+    return sb.toString();
+  }
+
+  private String retrieveMatchedContent(Issue issue, String content) {
+    String[] split = content.split("\\R");
+    StringBuilder sb = new StringBuilder();
+    int startLine = issue.primaryLocation().textRange().start().line();
+    int endLine = issue.primaryLocation().textRange().end().line();
+    for (int i = startLine; i <= endLine; i++) {
+      String stringToAdd = split[i - 1];
+      if (i == endLine) {
+        stringToAdd = stringToAdd.substring(0, issue.primaryLocation().textRange().end().lineOffset());
+      }
+      if (i == startLine) {
+        stringToAdd = stringToAdd.substring(issue.primaryLocation().textRange().start().lineOffset());
+      }
+      sb.append(stringToAdd);
+    }
+    return sb.toString();
   }
 
   public Check getInitializedCheck() {
