e336b06c8f1735ce5550cd2ecc5477a57c1d8236
Jonas Wielage <jonas.wielage@sonarsource.com>
2023-09-25 10:30:02 +0200

    Remove sonar-issues due to extended Quality Gate

--- sonar-text-plugin/src/main/java/org/sonar/plugins/secrets/api/SpecificationLoader.java
+++ sonar-text-plugin/src/main/java/org/sonar/plugins/secrets/api/SpecificationLoader.java
@@ -25,7 +25,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.sonar.plugins.secrets.configuration.deserialization.DeserializationException;
