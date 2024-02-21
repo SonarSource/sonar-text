cf7e1c1d464bda2185a306f6c1fa909ae502ddbf
Jonas Wielage <jonas.wielage@sonarsource.com>
2024-02-21 15:31:19 +0100

    SONARTEXT-119 Implement ThreadManager to manage all parallelization efforts (#86)

---
+++ sonar-text-plugin/src/main/java/org/sonar/plugins/common/thread/ParallelizationManager.java
@@ -0,0 +1,105 @@
+/*
+ * SonarQube Text Plugin
+ * Copyright (C) 2021-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.plugins.common.thread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ParallelizationManager {
+
+  private static final Logger LOG = LoggerFactory.getLogger(ParallelizationManager.class);
+  private static final int SEMAPHORE_ACQUIRE_TIMEOUT = 100;
+  private final int threads;
+  private final ExecutorService executorService;
+  private final Semaphore semaphore;
+  private final AtomicReference<Exception> exception = new AtomicReference<>();
+
+  public ParallelizationManager(int threads) {
+    this.threads = threads;
+    this.executorService = Executors.newFixedThreadPool(threads);
+    semaphore = new Semaphore(threads);
+  }
+
+  public void submit(Runnable runnable) {
+    if (executorService.isShutdown()) {
+      throw new RejectedExecutionException();
+    }
+
+    try {
+      boolean acquired;
+      do {
+        acquired = semaphore.tryAcquire(SEMAPHORE_ACQUIRE_TIMEOUT, TimeUnit.MILLISECONDS);
+        propagatePossibleException();
+      } while (!acquired);
+
+      executorService.execute(() -> {
+        try {
+          runnable.run();
+          semaphore.release();
+        } catch (Exception e) {
+          LOG.warn("Exception in thread " + Thread.currentThread().getName(), e);
+          exception.getAndSet(e);
+        }
+      });
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+    }
+  }
+
+  public void drainThreads() {
+    try {
+      boolean acquired;
+      do {
+        acquired = semaphore.tryAcquire(threads, SEMAPHORE_ACQUIRE_TIMEOUT, TimeUnit.MILLISECONDS);
+        propagatePossibleException();
+      } while (!acquired);
+      semaphore.release(threads);
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+    }
+  }
+
+  public void shutdown() {
+    executorService.shutdown();
+    try {
+      // we don't know how long threads will take to finish, so we wait for a long time
+      executorService.awaitTermination(1, TimeUnit.HOURS);
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+    }
+    propagatePossibleException();
+  }
+
+  private void propagatePossibleException() {
+    Throwable e = exception.getAndSet(null);
+    if (e != null) {
+      if (e instanceof IllegalStateException) {
+        throw (IllegalStateException) e;
+      }
+      throw new IllegalStateException(e);
+    }
+  }
+}
---
+++ sonar-text-plugin/src/test/java/org/sonar/plugins/common/thread/ParallelizationManagerTest.java
@@ -0,0 +1,166 @@
+/*
+ * SonarQube Text Plugin
+ * Copyright (C) 2021-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.plugins.common.thread;
+
+import java.util.List;
+import java.util.concurrent.RejectedExecutionException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.event.Level;
+import org.sonar.api.testfixtures.log.LogTesterJUnit5;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatNoException;
+
+class ParallelizationManagerTest {
+  private static final Logger LOG = LoggerFactory.getLogger(ParallelizationManagerTest.class);
+
+  @RegisterExtension
+  LogTesterJUnit5 logTester = new LogTesterJUnit5().setLevel(Level.DEBUG);
+
+  private ParallelizationManager executor = new ParallelizationManager(1);
+
+  private static final Runnable RUNNABLE_DOING_NOTHING = () -> {
+  };
+  private static final RuntimeException RUNTIME_EXCEPTION = new RuntimeException("msg");
+  private static final Runnable RUNNABLE_WITH_RUNTIME_EXCEPTION = () -> {
+    throw RUNTIME_EXCEPTION;
+  };
+
+  private static final IllegalStateException ILLEGAL_STATE_EXCEPTION = new IllegalStateException("msg");
+  private static final Runnable RUNNABLE_WITH_ILLEGAL_STATE_EXCEPTION = () -> {
+    throw ILLEGAL_STATE_EXCEPTION;
+  };
+
+  @BeforeEach
+  public void init() {
+    executor = new ParallelizationManager(1);
+  }
+
+  @AfterEach
+  public void cleanUp() {
+    executor.shutdown();
+  }
+
+  @Test
+  void shouldRunTasksSuccessFully() {
+    String expectedLogMessage = "Doing something";
+    Runnable runnableDoingSomething = () -> LOG.debug(expectedLogMessage);
+
+    executor.submit(runnableDoingSomething);
+    executor.submit(runnableDoingSomething);
+    executor.submit(runnableDoingSomething);
+
+    // wait for all threads to finish
+    executor.drainThreads();
+
+    List<String> logs = logTester.logs(Level.DEBUG);
+    assertThat(logs)
+      .hasSize(3)
+      .containsOnly(expectedLogMessage);
+  }
+
+  @Test
+  void shouldPropagateExceptionWhenSubmittingNewTaskAfterRunnableCrashedPreviously() {
+
+    executor.submit(RUNNABLE_DOING_NOTHING);
+    executor.submit(RUNNABLE_WITH_RUNTIME_EXCEPTION);
+
+    // as the previous task should exit with an exception, this task should propagate the exception
+    assertThatExceptionOfType(IllegalStateException.class)
+      .isThrownBy(() -> executor.submit(RUNNABLE_DOING_NOTHING))
+      .withCause(RUNTIME_EXCEPTION);
+  }
+
+  @Test
+  void shouldDrainSuccessfully() {
+    executor.submit(RUNNABLE_DOING_NOTHING);
+    assertThatNoException().isThrownBy(() -> executor.drainThreads());
+  }
+
+  @Test
+  void shouldPropagateExceptionWhenTerminatingThreadPoolAfterRunnableCrashedPreviously() {
+    executor.submit(RUNNABLE_WITH_RUNTIME_EXCEPTION);
+
+    // as the previous task should exit with an exception, this task should propagate the exception
+    assertThatExceptionOfType(IllegalStateException.class).isThrownBy(executor::shutdown).withCause(RUNTIME_EXCEPTION);
+  }
+
+  @Test
+  void shouldRejectRunnableWhenAlreadyShutdown() {
+    executor.shutdown();
+
+    // as the previous task should exit with an exception, this task should propagate the exception
+    assertThatExceptionOfType(RejectedExecutionException.class).isThrownBy(
+      () -> executor.submit(RUNNABLE_DOING_NOTHING));
+  }
+
+  @Test
+  void shouldPropagateIllegalStateException() {
+    executor.submit(RUNNABLE_DOING_NOTHING);
+    executor.submit(RUNNABLE_WITH_ILLEGAL_STATE_EXCEPTION);
+
+    // as the previous runnable should crash with an exception, this task should propagate the exception
+    assertThatExceptionOfType(IllegalStateException.class)
+      .isThrownBy(() -> executor.submit(RUNNABLE_DOING_NOTHING))
+      .withMessage(ILLEGAL_STATE_EXCEPTION.getMessage());
+  }
+
+  @Test
+  void shouldPropagateIllegalStateExceptionWhenDraining() {
+    executor.submit(RUNNABLE_DOING_NOTHING);
+    executor.submit(RUNNABLE_WITH_ILLEGAL_STATE_EXCEPTION);
+
+    // as the previous runnable should crash with an exception, this task should propagate the exception
+    assertThatExceptionOfType(IllegalStateException.class)
+      .isThrownBy(executor::drainThreads)
+      .withMessage(ILLEGAL_STATE_EXCEPTION.getMessage());
+  }
+
+  @Test
+  void shouldPropagateIllegalStateExceptionWhenTerminatingThreadPool() {
+    executor.submit(RUNNABLE_DOING_NOTHING);
+    executor.submit(RUNNABLE_WITH_ILLEGAL_STATE_EXCEPTION);
+
+    // as the previous runnable should crash with an exception, this task should propagate the exception
+    assertThatExceptionOfType(IllegalStateException.class)
+      .isThrownBy(executor::shutdown)
+      .withMessage(ILLEGAL_STATE_EXCEPTION.getMessage());
+  }
+
+  @Test
+  void interruptShouldBePropagatedOnSubmitNewRunnable() {
+    Thread.currentThread().interrupt();
+    executor.submit(RUNNABLE_DOING_NOTHING);
+    assertThat(Thread.interrupted()).isTrue();
+  }
+
+  @Test
+  void interruptShouldBePropagatedOnDrainingThreads() {
+    Thread.currentThread().interrupt();
+    executor.drainThreads();
+    assertThat(Thread.interrupted()).isTrue();
+  }
+}
