27a9fae3d12687ee610d0149d39b1696136fb46e
Julien Carsique <julien.carsique@sonarsource.com>
2024-02-28 13:30:03 +0100

    BUILD-4413 build number workaround (#127)

--- .cirrus.star
+++ .cirrus.star
@@ -6,6 +6,17 @@ def main(ctx):
     if env.get("CIRRUS_REPO_FULL_NAME") == 'SonarSource/sonar-text-enterprise':
         features = yaml.dumps(load_features(ctx, only_if=dict()))
         doc = fs.read("private/.cirrus.yml")
-        return features + doc
     else:
-        return fs.read(".cirrus-public.yml")
+        if env.get("CIRRUS_USER_PERMISSION") in ["write", "admin"]:
+            features = yaml.dumps(load_features(ctx, features=["build_number"]))
+        else:
+            # workaround for BUILD-4413 (build number on public CI)
+            features = yaml.dumps(
+                {
+                    'env': {
+                        'CI_BUILD_NUMBER': env.get("CIRRUS_PR", "1")
+                    },
+                }
+            )
+        doc = fs.read(".cirrus-public.yml")
+    return features + doc
--- build.sh
+++ build.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 set -euo pipefail
 
-: "${CIRRUS_BUILD_ID?}" "${CIRRUS_REPO_FULL_NAME?}" "${SONAR_HOST_URL?}" "${SONAR_TOKEN?}"
+: "${SONAR_HOST_URL?}" "${SONAR_TOKEN?}"
+: "${CI_BUILD_NUMBER?}" "${CIRRUS_BUILD_ID?}" "${CIRRUS_REPO_FULL_NAME?}" "${CIRRUS_CHANGE_IN_REPO?}"
 
 INITIAL_VERSION=$(grep version gradle.properties | awk -F= '{print $2}')
 
 ./gradlew --no-daemon --console plain \
-  -DbuildNumber="$CIRRUS_BUILD_ID" \
+  -DbuildNumber="$CI_BUILD_NUMBER" \
   build sonarqube \
   -Dsonar.host.url="$SONAR_HOST_URL" \
   -Dsonar.login="$SONAR_TOKEN" \
   -Dsonar.projectVersion="$INITIAL_VERSION" \
-  -Dsonar.analysis.buildNumber="$CIRRUS_BUILD_ID" \
+  -Dsonar.analysis.buildNumber="$CI_BUILD_NUMBER" \
   -Dsonar.analysis.pipeline="$CIRRUS_BUILD_ID" \
-  -Dsonar.analysis.sha1="${CIRRUS_BASE_SHA:-}" \
+  -Dsonar.analysis.sha1="$CIRRUS_CHANGE_IN_REPO" \
   -Dsonar.analysis.repository="$CIRRUS_REPO_FULL_NAME" \
   -Dsonar.organization=sonarsource
